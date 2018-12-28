4149f2e143a27b15cedb39bef0c2946158955ade
RKal-El <rmarking49@gmail.com>
2018-12-29 00:29:42 +0100

    Na toolbar traci dodani su sljedeci elementi: ime app (lijeva strana) te drop down menu (desna strana i bez funkcionalnosti)

--- app/src/main/java/com/example/marko/justgo/Profile.java
+++ app/src/main/java/com/example/marko/justgo/Profile.java
@@ -2,6 +2,12 @@ package com.example.marko.justgo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 
 public class Profile extends AppCompatActivity {
 
@@ -9,5 +15,36 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profile_toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    // Omogucuje da se vidi menu na custom toolbar (tri tockice)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // Omogucava da se preko menu-a ide na druge activity
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.profile_menu:
+                Toast.makeText(getApplicationContext(), "Profile", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.my_trips_menu:
+                Toast.makeText(getApplicationContext(), "My trips", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.utilities_menu:
+                Toast.makeText(getApplicationContext(), "Utilities", Toast.LENGTH_SHORT).show();
+                break;
+                default:
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
